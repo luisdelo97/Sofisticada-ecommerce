4bd122fd16bf1c5f18f181c52231f52a4ec5d413
Luis Delorenzi <ldelorenzi@fiuna.edu.py>
2024-01-12 15:29:26 -0300

    first commit

---
+++ src/app/theme.ts
@@ -0,0 +1,9 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const colors = {
+  brand: {
+    primary: "hsl(17, 70%, 63.90%)",
+    primaryLight: "hsl(17, 70%, 74%)",
+    primaryDark: "hsl(17, 70%, 54%)",
+  },
+};
