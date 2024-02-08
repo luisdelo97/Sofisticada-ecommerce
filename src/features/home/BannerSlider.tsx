169ada57becad7d0fe7ca3d0e4b54cd0885114da
Luis Delorenzi <ldelorenzi@fiuna.edu.py>
2024-02-08 11:14:57 -0300

    resolving the swiper import

--- src/features/home/BannerSlider.tsx
+++ src/features/home/BannerSlider.tsx
@@ -1,23 +1,23 @@
-'use client';
-import { SwiperOptions } from 'swiper/types/swiper-options';
-import { A11y, Autoplay, Navigation } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Box } from '@chakra-ui/react';
-import { imageBox } from './Styles';
-import { CSSProperties } from 'react';
+"use client";
+import { SwiperOptions } from "swiper/types";
+import { A11y, Autoplay, Navigation } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Box } from "@chakra-ui/react";
+import { imageBox } from "./Styles";
+import { CSSProperties } from "react";
 
 const slideStyles: CSSProperties = {
-  padding: '2rem 0.5rem',
-  boxSizing: 'border-box',
-  textAlign: 'center',
-  background: 'white',
+  padding: "2rem 0.5rem",
+  boxSizing: "border-box",
+  textAlign: "center",
+  background: "white",
 };
 
 export const BannerSlider = () => {
   const sliderSettings: SwiperOptions = {
     modules: [Navigation, A11y, Autoplay],
     spaceBetween: 10,
-    slidesPerView: 'auto',
+    slidesPerView: "auto",
     centeredSlides: true,
     loop: true,
     speed: 3000,
@@ -31,7 +31,7 @@ export const BannerSlider = () => {
     <Box w="100%" h="100%">
       <Swiper
         {...sliderSettings}
-        style={{ width: '100%', height: '100%', display: 'flex' }}
+        style={{ width: "100%", height: "100%", display: "flex" }}
       >
         <SwiperSlide style={slideStyles}>
           <Box bg="center / cover no-repeat url(product1.jpg)" {...imageBox} />
